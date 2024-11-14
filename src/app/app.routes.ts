dbbaf2ec7e45bd8be708c96122b4cbdf8da1e8f8
V.S.Naaviinesh <naaviinesh.v.s@gmail.com>
2024-11-15 00:26:02 +0800

    Initial commit of CrowdTrades - Under Contruction

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: 'Home', component: HomeComponent},
+    {path: 'About', component: AboutComponent},
+    {path: 'Contact', component: ContactComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent},
+   
+];
